afce875f5eb06d87f832d9024add4db47329e103
nabreesBM <137473584+nabreesBM@users.noreply.github.com>
2024-10-07 21:48:45 +0400

    Add files via upload

---
+++ style.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#arrow").click(function(){
+      $("#other").slideToggle("slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#menubar").click(function(){
+      $("#navbar").slideToggle("slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#downarrow").click(function(){
+      $("#cat").slideToggle("slow");
+    });
+  });
